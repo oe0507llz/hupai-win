946c4d4caa03cd910ddd5d8b67d843dd32952edb
Luozhou Li <luozhou@Luozhous-MacBook-Pro.local>
2019-05-14 20:31:18 -0400

    template_matching

--- hupai_win.py
+++ hupai_win.py
@@ -1,4 +1,4 @@
-#import numpy as np
+import numpy as np
 import pyautogui
 import time
 import os
@@ -11,6 +11,7 @@ from selenium import webdriver
 import time
 from datetime import datetime
 from dateutil import tz
+import cv2
 
 from_zone = tz.gettz('UTC')
 
@@ -25,6 +26,55 @@ local = utc.astimezone(to_zone)
 initial_time = datetime.strftime(local, "%H:%M:%S")
 
 print(initial_time)
+
+# Specify a threshold
+threshold = 0.75
+
+def template_matching(img_gray, template_image):
+
+    # Read the template
+    template = cv2.imread(template_image,0)
+ 
+    # Store width and heigth of template in w and h
+    w, h = template.shape[::-1]
+ 
+    # Perform match operations.
+    res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+
+    # Store the coordinates of matched area in a numpy array
+    loc = np.where( res >= threshold)
+    #print(loc)
+    if loc[0].size > 0:
+        return loc[1][0], loc[0][0], w, h
+    else:
+        return 0, 0, w, h
+
+def price_recognition(img_gray, template_image, relative_h, relative_w):
+    loc1_x, loc1_y, w1, h1 = template_matching(img_gray, template_image)
+    print("{}, {}, {}, {}".format(loc1_x, loc1_y, w1, h1))
+    if loc1_x > 0:
+        crop_img1 = img_gray[int(loc1_y+h1-h1*relative_h):loc1_y+h1, loc1_x+w1:int(loc1_x+w1+w1*relative_w)]
+        cv2.imwrite('{}{}/{}_crop_{}.png'.format(my_dir, fn, fn, str(datetime.today()).replace(" ", "")), crop_img1)
+        text1 = pytesseract.image_to_string(crop_img1, lang='eng', config = '-c tessedit_char_whitelist=0123456789')
+        print(text1)
+        text1_s = ''.join(i for i in text1 if i.isdigit())
+        print(text1_s)
+        for i in range(len(text1_s)):
+            if text1_s[i] == "8" or text1_s[i] == "9":
+                #print(i)
+                text1_r = text1_s[i:]
+                break
+            else:
+                text1_r = text1_s
+        if len(text1_r) > 5:
+            int_text1 = int(text1_r[0:5])
+        elif len(text1_r) == 4:
+            int_text1 = int(text1_r)*10
+        elif len(text1_r) == 3:
+            int_text1 = int(text1_r)*100
+        else:
+            int_text1 = int(text1_r)
+        return int_text1
 
 #url = 'http://moni.51hupai.com/'
 
@@ -91,30 +141,49 @@ if initial_time>'09:20:00':
         if not os.path.exists(new_dir):
             os.makedirs(new_dir)
 
-        area1 = (1144 + x_shift, 435 + y_shift, 1232 + x_shift, 465 + y_shift) # Lowest acceptable price
-        cropped_img1 = img.crop(area1)
-        cropped_img1.save('{}\{}_cropped1.png'.format(new_dir, fn))
-#        im = cropped_img1.convert('RGBA')
-#        data = np.array(im)
-#        rgb = data[:,:,:3]
-#        color1 = [251,237,227]
-#        color2 = [251,237,226]
-#        black = [0,0,0,255]
-#        white = [255,255,255,255]
-#        mask1 = np.all(rgb == color1, axis = -1)
-#        data[mask1] = white
-#        mask2 = np.all(rgb == color2, axis = -1)
-#        data[mask2] = white
-#        new_im = Image.fromarray(data)
-#        new_im.save('{}\{}_new_file.tif'.format(new_dir, fn))
-#        text1 = pytesseract.image_to_string(new_im, lang='eng', config = '-c tessedit_char_whitelist=0123456789')
-        text1 = pytesseract.image_to_string(cropped_img1, lang='eng', config = '-c tessedit_char_whitelist=0123456789')
-		if len(text1.replace(" ", "")) == 4:
-			int_text1 = int(text1.replace(" ", ""))*10
-		else:
-			int_text1 = int(text1)
-        lowest.append(text1)
-        #print(text1)
+        print("Round {}".format(i))
+        # Convert it to grayscale
+        img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+        #cv2.imwrite('{}_gray.png'.format(new_dir),img_gray)
+    
+        time.sleep(0.2)
+    
+        int_text1 = price_recognition(img_gray, 'template1.png', 1, 0.5)
+        print(int_text1)
+    
+        int_text1a_pre = price_recognition(img_gray, 'template1a.png', 1, 0.3)
+        if int_text1a_pre:
+            int_text1a = int_text1a_pre + 300
+        else:
+            int_text1a = int_text1a_pre
+        print(int_text1a)
+    
+        int_text1b = price_recognition(img_gray, 'template1b.png', 2.3, 1.5)
+        print(int_text1b)
+    
+        if int_text1 == int_text1a or int_text1 == int_text1b:
+            lowest_price = int_text1
+        elif int_text1a == int_text1b:
+            lowest_price = int_text1a
+        elif int_text1:
+            lowest_price = int_text1
+        elif int_text1b:
+            lowest_price = int_text1b
+        else:
+            lowest_price = int_text1a
+    
+        print("Lowest Transaction Pirce is {}".format(lowest_price))
+
+#        area1 = (1144 + x_shift, 435 + y_shift, 1232 + x_shift, 465 + y_shift) # Lowest acceptable price
+#        cropped_img1 = img.crop(area1)
+#        cropped_img1.save('{}\{}_cropped1.png'.format(new_dir, fn))
+#        text1 = pytesseract.image_to_string(cropped_img1, lang='eng', config = '-c tessedit_char_whitelist=0123456789')
+#		if len(text1.replace(" ", "")) == 4:
+#			int_text1 = int(text1.replace(" ", ""))*10
+#		else:
+#			int_text1 = int(text1)
+#        lowest.append(text1)
+#        #print(text1)
         utc = datetime.utcnow()
         print(utc)
         utc = utc.replace(tzinfo=from_zone)
@@ -122,11 +191,11 @@ if initial_time>'09:20:00':
         text2 = datetime.strftime(local, "%H:%M:%S")
         #timeNow.append(text2)
         print(text2)
-        print(text1)
+        print(lowest_price)
     	
         if text2 == "11:29:44" or text2 == "11:29:45":
     	#if text2 == "11:29:44" or text2 == "11:29:45":
-            number_int = int_text1 +900
+            number_int = lowest_price +900
             number = str(number_int)
             
             pyautogui.moveTo(1204 + x_shift, 704 + y_shift)
